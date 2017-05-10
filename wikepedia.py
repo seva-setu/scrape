8686316b8374da330b7474ba8045f87ac674fd9a
r3dt3am <r3dt3am@localhost.localdomain>
2017-05-09 22:41:41 -0400

    removed bug in first link

--- wikepedia.py
+++ wikepedia.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request,urlopen
-import csv	
+import csv
 import pandas as pd
 
 wiki = "https://en.wikipedia.org/wiki/List_of_government_schemes_in_India"
@@ -26,26 +26,26 @@ table = soup.find("table", { "class" : "wikitable sortable" })
 
 
 for row in table.findAll("tr"):
-    cells = row.findAll("td")
+	cells = row.findAll("td")
     #For each "tr", assign each "td" to a variable.
-    
-    if len(cells) == 6:
-    	links_res = cells[0].findAll('a', href=True)
-    	for res in links_res:
-    		link = "https://en.wikepedia.org" + res.get('href')
-            if '#' in link:
-                link = ''
+	if len(cells) == 6:
+		links_res = cells[0].findAll('a', href=True)
+		for res in links_res:
+			link = "https://en.wikepedia.org" + res.get('href')
+			if '#' in link:
+				link = ''
 
-    	title = cells[0].find(text=True) 
-    	ministry = cells[1].find(text=True)
-    	sector = cells[4].find(text=True)   #to be added under benefits column
-    	detail = cells[5].find(text=True)
+		title = cells[0].find(text=True)
+		ministry = cells[1].find(text=True)
+		sector = cells[4].find(text=True)   #to be added under benefits column
+		detail = cells[5].find(text=True)
 
-    	data['_title'].append(title)
-    	data['link'].append(link)
-    	data['detail'].append(detail)
-    	data['ministry'].append(ministry)
-    	data['sector'].append(sector)
+
+		data['_title'].append(title)
+		data['link'].append(link)
+		data['detail'].append(detail)
+		data['ministry'].append(ministry)
+		data['sector'].append(sector)
 
 
 dogData = pd.DataFrame( data )
